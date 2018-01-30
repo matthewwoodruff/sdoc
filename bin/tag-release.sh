c527e2e6b0b77b274894f042013b283221aed3dc
Matthew Woodruff <mwoodruf@thoughtworks.com>
2018-01-30 21:32:15 +0000

    Update tag release script so that version releases are signed

--- bin/tag-release.sh
+++ bin/tag-release.sh
@@ -1,11 +1,21 @@
 #! /bin/bash
 
-set -evu
+set -eu
 
 version=${1:?First argument is the version you want to tag, like '1.0.0'}
+version_tag="v$version"
+
+if git rev-parse "$version_tag" >/dev/null 2>&1
+then
+  echo "Version $version_tag already exists"
+  exit 1
+fi
 
 sed -i "" -E "s/^version = \".*\"/version = \"$version\"/" Cargo.toml
 
-git tag "v$version"
+git reset .
+git add Cargo.toml
+git commit -S -m "Release $version_tag"
+git tag -s "$version_tag" -m "Release $version_tag"
 
 exit 0
