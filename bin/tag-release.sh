8d385948ee7b2aef9cfb036f134eff524fad92f5
Matthew Woodruff <mwoodruf@thoughtworks.com>
2018-01-30 20:29:20 +0000

    Publish to crates.io on tags

--- bin/tag-release.sh
+++ bin/tag-release.sh
@@ -6,7 +6,6 @@ version=${1:?First argument is the version you want to tag, like '1.0.0'}
 
 sed -i "" -E "s/^version = \".*\"/version = \"$version\"/" Cargo.toml
 
-git commit -am "Release v$version"
 git tag "v$version"
 
 exit 0
