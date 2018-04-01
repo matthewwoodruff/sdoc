f237d7e88fb625017819f76855fc9927468c0e57
Matthew Woodruff <matthew.woodruff+github@me.com>
2018-04-01 16:40:03 +0100

    Release v0.4.2 through homebrew

--- bin/build.sh
+++ bin/build.sh
@@ -8,14 +8,14 @@ while getopts ":q" opt; do
 done
 shift $((OPTIND-1))
 
-declare target=$TARGET
+declare target=${TARGET}
 default_target=$(head -n 1 <<<$(rustup show) | cut -d ' ' -f 3)
 
 echo "Default target is: $default_target"
 
 if [[ -z "$target" ]];
 then
-  target=$default_target
+  target=${default_target}
 elif [[ "$target" != "$default_target" ]];
 then
   rustup target add $target
@@ -23,20 +23,24 @@ fi
 
 full_build_type=$(test -z "$quick_build" && echo 'release' || echo 'debug')
 
-echo Building $full_build_type for $target
+echo Building ${full_build_type} for ${target}
 
-declare build_path=target/$target/$full_build_type
-build_arg=$(test -z "$quick_build" && echo '--release' || echo -n '') 
+declare build_path=target/${target}/${full_build_type}
+declare build_arg=$(test -z "$quick_build" && echo '--release' || echo -n '')
 
-cargo test --all $build_arg --target $target
-cargo build $build_arg --target $target
-./snapshot-tests/run.sh $(test -n "$quick_build" && echo '-d' || echo -n '') $build_path/sdoc
+cargo test --all ${build_arg} --target ${target}
+cargo build ${build_arg} --target ${target}
+./snapshot-tests/run.sh $(test -n "$quick_build" && echo '-d' || echo -n '') ${build_path}/sdoc
 
 if [[ -z "$quick_build" ]];
 then
   mkdir -p dist
   declare build=${TRAVIS_TAG:-SNAPSHOT}
-  tar czf dist/sdoc-$build-$target.tar.gz -C $build_path sdoc
+  declare package="dist/sdoc-$build-$target.tar.gz"
+  tar czf ${package} -C ${build_path} sdoc
+  echo
+  echo 'Checksum'
+  shasum -a 256 ${package}
 fi
 
 exit 0
--- pkg/brew/sdoc.rb
+++ pkg/brew/sdoc.rb
@@ -1,11 +1,11 @@
 class Sdoc < Formula
-version '0.4.0'
+  version '0.4.2'
   desc "Framework for building custom CLIs around shell, scripts, and executables"
   homepage "https://github.com/matthewwoodruff/sdoc"
 
   if OS.mac?
       url "https://github.com/matthewwoodruff/sdoc/releases/download/v#{version}/sdoc-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "12efbe89ef0f03bbc7ba05909c6ab0b8dd8c7a99c5293788bc9402059af54abd"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   end
 
   def install
