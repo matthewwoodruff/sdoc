685839472fff66b9010de408fb5dea8a9d6a1dcf
Matthew Woodruff <matthew.woodruff+github@me.com>
2018-04-08 19:26:26 +0100

    Release v0.5.0

--- pkg/brew/sdoc.rb
+++ pkg/brew/sdoc.rb
@@ -1,5 +1,5 @@
 class Sdoc < Formula
-  version '0.4.2'
+ version '0.5.0'
   desc "Framework for building custom CLIs around shell, scripts, and executables"
   homepage "https://github.com/matthewwoodruff/sdoc"
 
