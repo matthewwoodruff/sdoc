5d579a5d907703279537eea6097ca57b812fe9bd
Matthew Woodruff <matthew.woodruff@me.com>
2021-10-12 20:24:45 +0100

    Release 0.8.11 to homebrew

--- pkg/brew/sdoc.rb
+++ pkg/brew/sdoc.rb
@@ -1,11 +1,11 @@
 class Sdoc < Formula
- version '0.8.10'
+ version '0.8.11'
   desc "Framework for building custom CLIs around shell, scripts, and executables"
   homepage "https://github.com/matthewwoodruff/sdoc"
 
   if OS.mac?
       url "https://github.com/matthewwoodruff/sdoc/releases/download/v#{version}/sdoc-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "0703ead0de8bd367a80d9ea05bc48762acb0f739b6e4aaf84366c7cfd58ae0b9"
+      sha256 "0b8d5294d47d58dfac9e174922b4f94770fd6849504a017ceac4c0aa3f552fdb"
   end
 
   def install
